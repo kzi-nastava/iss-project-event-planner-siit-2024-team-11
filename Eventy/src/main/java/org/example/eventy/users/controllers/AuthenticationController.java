8eca1106d96486e40884b3d1498f7270bfcd321e
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-11-10 13:25:47 +0100

    [FIX] Authentication Controller now has an end point for registration confirmation

--- Eventy/src/main/java/org/example/eventy/users/controllers/AuthenticationController.java
+++ Eventy/src/main/java/org/example/eventy/users/controllers/AuthenticationController.java
@@ -5,10 +5,7 @@ import org.example.eventy.users.dtos.RegisterDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
@@ -27,9 +24,20 @@ public class AuthenticationController {
     @PostMapping(value = "register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> login(@RequestBody RegisterDTO registerDTO) {
         if(Objects.equals(registerDTO.getEmail(), "good@email.com")) {
-            return new ResponseEntity<String>("JWT Token", HttpStatus.OK);
+            return new ResponseEntity<String>(HttpStatus.OK);
         }
 
         return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping(value="register/{id}")
+    public ResponseEntity<String> confirmRegistration(@PathVariable Long id) {
+        if(id == 5) {
+            return new ResponseEntity<String>("JWT Token", HttpStatus.OK);
+            // redirect to the home page?
+        }
+
+        return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+        // redirect to some kind of error page?
     }
 }
