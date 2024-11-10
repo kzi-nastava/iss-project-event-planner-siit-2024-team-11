549955a3360425400a1ad92547e7767e2169d2a0
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-11-10 01:09:41 +0100

    Initial structure

---
+++ Eventy/src/main/java/org/example/eventy/EventyApplication.java
@@ -0,0 +1,13 @@
+package org.example.eventy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventyApplication.class, args);
+    }
+
+}
---
+++ Eventy/src/test/java/org/example/eventy/EventyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eventy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
