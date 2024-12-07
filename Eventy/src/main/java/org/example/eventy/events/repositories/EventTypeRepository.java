830b1f2ed93a8d7c5d4d75a48226260fed4a45cd
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-12-07 13:34:08 +0100

    [FIX] EventTypeRepository annotation

--- Eventy/src/main/java/org/example/eventy/events/repositories/EventTypeRepository.java
+++ Eventy/src/main/java/org/example/eventy/events/repositories/EventTypeRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EventTypeRepository extends JpaRepository<EventType, Long> {
     Page<EventType> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description, Pageable pageable);
     List<EventType> findByIsActiveTrue();
