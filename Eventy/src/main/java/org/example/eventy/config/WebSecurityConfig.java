1c64c98e8fd6b0ace945e2a1dc87cd646d96b690
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-12-11 22:22:46 +0100

    [FIX] Security config permit all paths updated

--- Eventy/src/main/java/org/example/eventy/config/WebSecurityConfig.java
+++ Eventy/src/main/java/org/example/eventy/config/WebSecurityConfig.java
@@ -81,10 +81,9 @@ public class WebSecurityConfig {
          http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
          http.exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(restAuthenticationEntryPoint));
          http.authorizeHttpRequests(request -> {
-             request.requestMatchers(new AntPathRequestMatcher("/auth/login")).permitAll()
-                    .requestMatchers(new AntPathRequestMatcher("/api/foo")).permitAll()
-                    //Da nam lepsu poruku vrati
-                    .requestMatchers(new AntPathRequestMatcher("/error")).permitAll()
+             request.requestMatchers(new AntPathRequestMatcher("/api/authentication/*")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/api/events")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/api/solutions")).permitAll()
                   //.requestMatchers(new AntPathRequestMatcher("/api/whoami")).hasRole("USER")
              .anyRequest().authenticated();
          });
