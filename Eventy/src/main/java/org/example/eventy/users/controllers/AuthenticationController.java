cd37f2b26293d610bb952f2da86e2fb6e1878fef
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-12-13 18:53:29 +0100

    [FEAT] User registration, without JWT for now

--- Eventy/src/main/java/org/example/eventy/users/controllers/AuthenticationController.java
+++ Eventy/src/main/java/org/example/eventy/users/controllers/AuthenticationController.java
@@ -1,10 +1,15 @@
 package org.example.eventy.users.controllers;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.eventy.common.models.PicturePath;
 import org.example.eventy.common.services.EmailService;
+import org.example.eventy.common.services.PictureService;
 import org.example.eventy.users.dtos.*;
+import org.example.eventy.users.models.EventOrganizer;
 import org.example.eventy.users.models.RegistrationRequest;
+import org.example.eventy.users.models.SolutionProvider;
 import org.example.eventy.users.models.User;
+import org.example.eventy.users.repositories.RoleRepository;
 import org.example.eventy.users.services.RegistrationRequestService;
 import org.example.eventy.users.services.UserService;
 import org.example.eventy.util.TokenUtils;
@@ -16,9 +21,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/authentication")
@@ -37,6 +45,9 @@ public class AuthenticationController {
 
     @Autowired
     private EmailService emailService;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserTokenState> login(@RequestBody LoginDTO authenticationRequest, HttpServletResponse response) {
@@ -60,11 +71,77 @@ public class AuthenticationController {
 
     @PostMapping(value = "/registration", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> register(@RequestBody RegistrationDTO registrationDTO) {
-        if(registrationDTO.getEmail().equals("good@gmail.com")) {
-            return new ResponseEntity<String>("Confirmation email sent to the email address!", HttpStatus.CREATED);
+        User user;
+        if(registrationDTO.getName() == null) {
+            EventOrganizer newOrganizer = new EventOrganizer();
+            newOrganizer.setFirstName(registrationDTO.getFirstName());
+            newOrganizer.setLastName(registrationDTO.getLastName());
+            newOrganizer.setPassword(registrationDTO.getPassword());
+            newOrganizer.setEmail(registrationDTO.getEmail());
+            newOrganizer.setAddress(registrationDTO.getAddress());
+            newOrganizer.setPhoneNumber(registrationDTO.getPhoneNumber());
+            newOrganizer.setActive(false);
+            newOrganizer.setDeactivated(false);
+            newOrganizer.setHasSilencedNotifications(false);
+            newOrganizer.setRole(roleRepository.findByName("Organizer"));
+            List<PicturePath> profilePictures = new ArrayList<>();
+            for(String path : registrationDTO.getProfilePictures()) {
+                PicturePath picturePath = new PicturePath();
+                picturePath.setPath(path);
+                profilePictures.add(picturePath);
+                // no need for PictureService right?
+            }
+            newOrganizer.setImageUrls(profilePictures);
+
+            newOrganizer = (EventOrganizer) userService.save(newOrganizer);
+            if(newOrganizer == null) {
+                return new ResponseEntity<String>("Validation failed!", HttpStatus.BAD_REQUEST);
+            }
+
+            user = newOrganizer;
+        }
+        else {
+            SolutionProvider newProvider = new SolutionProvider();
+            newProvider.setName(registrationDTO.getName());
+            newProvider.setDescription(registrationDTO.getDescription());
+            newProvider.setPassword(registrationDTO.getPassword());
+            newProvider.setEmail(registrationDTO.getEmail());
+            newProvider.setAddress(registrationDTO.getAddress());
+            newProvider.setPhoneNumber(registrationDTO.getPhoneNumber());
+            newProvider.setActive(false);
+            newProvider.setDeactivated(false);
+            newProvider.setHasSilencedNotifications(false);
+            newProvider.setRole(roleRepository.findByName("Provider"));
+            List<PicturePath> profilePictures = new ArrayList<>();
+            for(String path : registrationDTO.getProfilePictures()) {
+                PicturePath picturePath = new PicturePath();
+                picturePath.setPath(path);
+                profilePictures.add(picturePath);
+                // no need for PictureService right?
+            }
+            newProvider.setImageUrls(profilePictures);
+
+            newProvider = (SolutionProvider) userService.save(newProvider);
+            if(newProvider == null) {
+                return new ResponseEntity<String>("Validation failed!", HttpStatus.BAD_REQUEST);
+            }
+
+            user = newProvider;
         }
 
-        return new ResponseEntity<String>("Validation failed!", HttpStatus.BAD_REQUEST);
+        RegistrationRequest registrationRequest = registrationRequestService.create(user);
+        if(registrationRequest == null) {
+            return new ResponseEntity<String>("Creating request failed!", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            emailService.sendEmail(user.getEmail(), "Confirm registration", "Click on this link to confirm registration: localhost:8080/api/authentication/registration-confirmation/" + registrationRequest.getId());
+        }
+        catch (Exception e) {
+            return new ResponseEntity<String>("Sending email failed!", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<String>("Confirmation email sent to the email address!", HttpStatus.CREATED);
     }
 
     @PutMapping(value="/registration-confirmation/{requestId}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -75,7 +152,19 @@ public class AuthenticationController {
             return new ResponseEntity<UserDTO>(HttpStatus.NOT_FOUND);
         }
         else if(status == RequestUpdateStatus.TOO_LATE) {
-            // maybe send new reg request?
+            User user = registrationRequestService.getUserForRequest(requestId);
+
+            RegistrationRequest registrationRequest = registrationRequestService.create(user);
+            if(registrationRequest == null) {
+                return new ResponseEntity<UserDTO>(HttpStatus.BAD_REQUEST);
+            }
+
+            try {
+                emailService.sendEmail(user.getEmail(), "Confirm registration", "Click on this link to confirm registration: localhost:8080/api/authentication/registration-confirmation/" + registrationRequest.getId());
+            }
+            catch (Exception e) {
+                return new ResponseEntity<UserDTO>(HttpStatus.BAD_REQUEST);
+            }
             return new ResponseEntity<UserDTO>(HttpStatus.BAD_REQUEST);
         }
 
--- Eventy/src/main/java/org/example/eventy/users/repositories/RoleRepository.java
+++ Eventy/src/main/java/org/example/eventy/users/repositories/RoleRepository.java
@@ -3,8 +3,6 @@ package org.example.eventy.users.repositories;
 import org.example.eventy.users.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    List<Role> findByName(String name);
+    Role findByName(String name);
 }
