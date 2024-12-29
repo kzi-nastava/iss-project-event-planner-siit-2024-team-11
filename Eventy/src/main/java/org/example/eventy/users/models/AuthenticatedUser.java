c175abf80045806004399bfb223c516ea1199c8c
Tamara Cvjetkovic <c.tamara02@gmail.com>
2024-12-30 00:32:04 +0100

    [REFACTOR] AuthenticatedUser Model (delete first and last name)

--- Eventy/src/main/java/org/example/eventy/users/models/AuthenticatedUser.java
+++ Eventy/src/main/java/org/example/eventy/users/models/AuthenticatedUser.java
@@ -7,33 +7,8 @@ import jakarta.persistence.Entity;
 @Entity
 @DiscriminatorValue("AuthenticatedUser")
 public class AuthenticatedUser extends User {
-    @Column()
-    private String firstName;
-    @Column()
-    private String lastName;
 
     public AuthenticatedUser() {
 
-    }
-
-    public AuthenticatedUser(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 }
