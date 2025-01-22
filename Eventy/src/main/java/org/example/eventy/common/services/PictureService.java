f695de3a8cf45b3b8e670c85c3cd255284bd2a63
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-01-22 18:18:12 +0100

    [FEAT] Picture service saving pictures first update

--- Eventy/src/main/java/org/example/eventy/common/services/PictureService.java
+++ Eventy/src/main/java/org/example/eventy/common/services/PictureService.java
@@ -4,7 +4,15 @@ import org.example.eventy.common.models.PicturePath;
 import org.example.eventy.common.repositories.PictureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,23 +21,64 @@ public class PictureService {
     @Autowired
     private PictureRepository pictureRepository;
 
+    private final String uploadDir = "src/main/resources/static";
+    private int lastPathNumber = 0;
+
+    public PictureService() {
+        File pictureDir = new File(uploadDir);
+        lastPathNumber = pictureDir.list().length;
+    }
+
     public List<PicturePath> save(List<String> paths) {
         List<PicturePath> picturePaths = new ArrayList<>();
 
         for(String path : paths) {
-            PicturePath picturePath = pictureRepository.findFirstByPath(path);
-
-            if(picturePath == null) {
-                picturePath = new PicturePath();
-                picturePath.setPath(path);
+            try {
+                PicturePath picturePath = new PicturePath();
+                picturePath.setPath(lastPathNumber + ".jpg");
+                this.saveImage(path);
                 picturePath = pictureRepository.save(picturePath);
-            }
-
-            if(picturePath != null) {
                 picturePaths.add(picturePath);
+            }
+            catch (Exception e) {
+                return null;
             }
         }
 
         return picturePaths;
+    }
+
+    public byte[] getImage(String path) {
+        File file = new File(path);
+
+        try {
+            return Files.readAllBytes(file.toPath());
+        }
+        catch (IOException e) {
+            return null;
+        }
+    }
+
+    private void saveImage(MultipartFile multipartFile) throws IOException {
+
+        //Kreiranje putanje direktorijuma
+        Path uploadPath = Paths.get(uploadDir);
+
+        //Provjera postojanja direktorijuma
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        //Čitanje sadržaja fajla i njegovo kopiranje datu fajl putanju
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(lastPathNumber + ".jpg"); //puna putanja na kojoj treba da se sačuva fajl
+
+            System.out.println("FilePath je:" + uploadPath.toAbsolutePath());
+
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            lastPathNumber++;
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + lastPathNumber + ".jpg", ioe);
+        }
     }
 }
