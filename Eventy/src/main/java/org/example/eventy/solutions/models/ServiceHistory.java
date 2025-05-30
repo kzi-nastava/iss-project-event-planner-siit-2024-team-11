9995c08499b220949d9ab9aa67b74b4ceb50f17d
Matijas Levang <micolevang@gmail.com>
2025-05-31 00:31:46 +0200

    [ADD] Service History repository and service

--- Eventy/src/main/java/org/example/eventy/solutions/models/ServiceHistory.java
+++ Eventy/src/main/java/org/example/eventy/solutions/models/ServiceHistory.java
@@ -22,48 +22,24 @@ public class ServiceHistory {
     @Column(nullable = false)
     private int discount;
 
-    @Column(nullable = false)
-    private String specifics;
-
-    @Column(nullable = false)
-    private Integer minReservationTime;
-
-    @Column(nullable = false)
-    private Integer maxReservationTime;
-
-    @Column(nullable = false)
-    private Integer reservationDeadline;
-
-    @Column(nullable = false)
-    private Integer cancellationDeadline;
-
-    @Column(nullable = false)
-    private ReservationConfirmationType reservationType;
-
-    @Column(nullable = false)
-    private boolean isVisible;
-
-    @Column(nullable = false)
-    private boolean isAvailable;
-
     public ServiceHistory() {
 
     }
 
-    public ServiceHistory(Long id, String name, String description, double price, int discount, String specifics, Integer minReservationTime, Integer maxReservationTime, Integer reservationDeadline, Integer cancellationDeadline, ReservationConfirmationType reservationType, boolean isVisible, boolean isAvailable) {
+    public ServiceHistory(Long id, String name, String description, double price, int discount) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.discount = discount;
-        this.specifics = specifics;
-        this.minReservationTime = minReservationTime;
-        this.maxReservationTime = maxReservationTime;
-        this.reservationDeadline = reservationDeadline;
-        this.cancellationDeadline = cancellationDeadline;
-        this.reservationType = reservationType;
-        this.isVisible = isVisible;
-        this.isAvailable = isAvailable;
+    }
+
+    public ServiceHistory(Service service) {
+        this.name = service.getName();
+        this.description = service.getDescription();
+        this.price = service.getPrice();
+        this.discount = service.getDiscount();
+
     }
 
     public Long getId() {
@@ -106,67 +82,4 @@ public class ServiceHistory {
         this.discount = discount;
     }
 
-    public String getSpecifics() {
-        return specifics;
-    }
-
-    public void setSpecifics(String specifics) {
-        this.specifics = specifics;
-    }
-
-    public Integer getMinReservationTime() {
-        return minReservationTime;
-    }
-
-    public void setMinReservationTime(Integer minReservationTime) {
-        this.minReservationTime = minReservationTime;
-    }
-
-    public Integer getMaxReservationTime() {
-        return maxReservationTime;
-    }
-
-    public void setMaxReservationTime(Integer maxReservationTime) {
-        this.maxReservationTime = maxReservationTime;
-    }
-
-    public Integer getReservationDeadline() {
-        return reservationDeadline;
-    }
-
-    public void setReservationDeadline(Integer reservationDeadline) {
-        this.reservationDeadline = reservationDeadline;
-    }
-
-    public Integer getCancellationDeadline() {
-        return cancellationDeadline;
-    }
-
-    public void setCancellationDeadline(Integer cancellationDeadline) {
-        this.cancellationDeadline = cancellationDeadline;
-    }
-
-    public ReservationConfirmationType getReservationType() {
-        return reservationType;
-    }
-
-    public void setReservationType(ReservationConfirmationType reservationType) {
-        this.reservationType = reservationType;
-    }
-
-    public boolean isVisible() {
-        return isVisible;
-    }
-
-    public void setVisible(boolean visible) {
-        isVisible = visible;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available) {
-        isAvailable = available;
-    }
 }
