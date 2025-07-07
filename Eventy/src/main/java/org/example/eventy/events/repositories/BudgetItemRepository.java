3c1cdc9c7247e340683509af064b422ce0726b71
Matijas Levang <micolevang@gmail.com>
2025-07-07 16:38:01 +0200

    [ADD] BudgetItemService functions

--- Eventy/src/main/java/org/example/eventy/events/repositories/BudgetItemRepository.java
+++ Eventy/src/main/java/org/example/eventy/events/repositories/BudgetItemRepository.java
@@ -1,7 +1,9 @@
 package org.example.eventy.events.repositories;
 
 import org.example.eventy.events.models.BudgetItem;
+import org.example.eventy.solutions.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BudgetItemRepository extends JpaRepository<BudgetItem, Long> {
+    boolean removeById(Long id);
 }
