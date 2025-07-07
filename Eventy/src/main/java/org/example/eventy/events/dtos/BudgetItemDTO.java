cf7a8330a676fdfea9cb45d5c4963c672c545d05
Matijas Levang <micolevang@gmail.com>
2025-07-07 16:33:28 +0200

    [CHANGE] BudgetItemDTO now works with SolutionDetailsDTOs and SolutionHistory

--- Eventy/src/main/java/org/example/eventy/events/dtos/BudgetItemDTO.java
+++ Eventy/src/main/java/org/example/eventy/events/dtos/BudgetItemDTO.java
@@ -2,23 +2,35 @@ package org.example.eventy.events.dtos;
 
 import org.example.eventy.events.models.BudgetItem;
 import org.example.eventy.solutions.dtos.SolutionCardDTO;
+import org.example.eventy.solutions.dtos.SolutionDetailsDTO;
+import org.example.eventy.solutions.services.SolutionService;
+import org.example.eventy.users.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BudgetItemDTO {
+    private Long id;
     private String category;
     private double plannedFunds;
-    private List<SolutionCardDTO> budgetedEntries;
+    private List<SolutionDetailsDTO> budgetedEntries;
 
     public BudgetItemDTO() {}
 
-    public BudgetItemDTO(BudgetItem budgetItem) {
+    public BudgetItemDTO(BudgetItem budgetItem, SolutionService solutionService, User loggedInUser) {
+        this.id = budgetItem.getId();
         this.category = budgetItem.getCategory().getName();
         this.plannedFunds = budgetItem.getPlannedFunds();
         this.budgetedEntries = new ArrayList<>();
-        // TO-DO: pass loggedInUser here
-        budgetItem.getReservedItems().forEach(v -> budgetedEntries.add(new SolutionCardDTO(v, null)));
+        budgetItem.getReservedItems().forEach(v -> budgetedEntries.add(new SolutionDetailsDTO(v, solutionService.getSolution(v.getSolutionId()),loggedInUser)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getCategory() {
@@ -37,11 +49,11 @@ public class BudgetItemDTO {
         this.plannedFunds = plannedFunds;
     }
 
-    public List<SolutionCardDTO> getBudgetedEntries() {
+    public List<SolutionDetailsDTO> getBudgetedEntries() {
         return budgetedEntries;
     }
 
-    public void setBudgetedEntries(List<SolutionCardDTO> budgetedEntries) {
+    public void setBudgetedEntries(List<SolutionDetailsDTO> budgetedEntries) {
         this.budgetedEntries = budgetedEntries;
     }
 }
